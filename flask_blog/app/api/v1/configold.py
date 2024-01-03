da90387b65be8cd983072fcd51a5097c07e7bade
Pascal Ojukwu <luvpascal.ojukwu@yahoo.com>
2024-01-03 22:16:58 +0100

    Delete flask_blog/app/api directory

--- flask_blog/app/api/v1/configold.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_url = os.path.dirname(os.path.abspath(__name__))
-class Config:
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = base_url
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_url, 'Database.db')
--- flask_blog/app/api/v1/views/__int__.py
+++
@@ -1,6 +0,0 @@
-"""init the package on load"""
-
-# from api.v1.views.posts import *
-# from api.v1.views.tags import *
-# from api.v1.views.users_posts import *
-# from api.v1.views.posts_tags import *s
